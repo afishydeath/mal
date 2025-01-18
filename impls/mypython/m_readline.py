338c58331f1334e792c0670f511de1b8507c0d8e
installer <installer@gmail.com>
2025-01-18 21:54:30 +1100

    new step 0 (likely not different)

---
+++ impls/mypython/m_readline.py
@@ -0,0 +1,18 @@
+import os
+import readline
+import atexit
+
+histfile = os.path.join(os.path.expanduser("~"), ".mal_history")
+
+try:
+    readline.read_history_file(histfile)
+    # default history len is -1 (infinite), which may grow unruly
+    readline.set_history_length(1000)
+except FileNotFoundError:
+    pass
+
+atexit.register(readline.write_history_file, histfile)
+
+
+def input_(*args, **kwargs):
+    return input(*args, **kwargs)
